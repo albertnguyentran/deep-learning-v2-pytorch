bd0aade7cb6a83d658e2f30a4da7a7210d213731
Shiza Charania <86497160+shizacharania@users.noreply.github.com>
2021-12-05 18:13:04 -0500

    Update helper.py

--- intro-to-pytorch/helper.py
+++ intro-to-pytorch/helper.py
@@ -64,13 +64,40 @@ def view_recon(img, recon):
         ax.axis('off')
         ax.set_adjustable('box-forced')
 
+# def view_classify(img, ps, version="MNIST"):
+#     ''' Function for viewing an image and it's predicted classes.
+#     '''
+#     ps = ps.data.numpy().squeeze()
+
+#     fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+#     ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+#     ax1.axis('off')
+#     ax2.barh(np.arange(10), ps)
+#     ax2.set_aspect(0.1)
+#     ax2.set_yticks(np.arange(10))
+#     if version == "MNIST":
+#         ax2.set_yticklabels(np.arange(10))
+#     elif version == "Fashion":
+#         ax2.set_yticklabels(['T-shirt/top',
+#                             'Trouser',
+#                             'Pullover',
+#                             'Dress',
+#                             'Coat',
+#                             'Sandal',
+#                             'Shirt',
+#                             'Sneaker',
+#                             'Bag',
+#                             'Ankle Boot'], size='small');
+#     ax2.set_title('Class Probability')
+#     ax2.set_xlim(0, 1.1)
+
+#     plt.tight_layout()
 def view_classify(img, ps, version="MNIST"):
     ''' Function for viewing an image and it's predicted classes.
     '''
     ps = ps.data.numpy().squeeze()
-
     fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze(), normalize=True)
     ax1.axis('off')
     ax2.barh(np.arange(10), ps)
     ax2.set_aspect(0.1)
@@ -90,5 +117,4 @@ def view_classify(img, ps, version="MNIST"):
                             'Ankle Boot'], size='small');
     ax2.set_title('Class Probability')
     ax2.set_xlim(0, 1.1)
-
     plt.tight_layout()
